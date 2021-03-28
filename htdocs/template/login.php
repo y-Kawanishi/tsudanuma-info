ef9a7003bf85981ded34b211177f86a601eb8d9e
y-Kawanishi <s1942031UM@s.chibakoudai.jp>
2021-03-29 01:25:21 +0900

    ログインページ レイアウト変更

--- htdocs/template/login.php
+++ htdocs/template/login.php
@@ -18,16 +18,35 @@ session_destroy();
     <meta charset="utf-8" />
         <!-- css -->
         <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
         <!-- bootstrap -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sign-in/">
         <!-- font -->
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Kosugi&family=Kosugi+Maru&display=swap" rel="stylesheet">
         <title>ログイン</title>
+        
+        <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+        
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+        </style>
+
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -84,17 +103,14 @@ session_destroy();
         <div id="wrapper" class="container w-50">
             <div id="page_top"><a href="#"></a></div>
             <section class="section">
-                <h1>ログイン</h1>
                 <form id="entry" method="POST" action="../account/login-check.php">
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">メールアドレス</label>
-                        <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">パスワード</label>
-                        <input type="password" name="pass" class="form-control" id="exampleInputPassword1">
-                    </div>
-                    <button type="submit" class="btn btn-primary">ログイン</button>
+                    <img class="mb-4" src="../img/つだぬまいんふぉ.png" alt="146" width="" height="97">
+                    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+                    <label for="exampleInputEmail1" class="form-label"></label>
+                    <input type="email" name="email" id="exampleInputEmail1" class="form-control" placeholder="Email address" required autofocus>
+                    <label for="exampleInputPassword1" class="form-label"></label>
+                    <input type="password" name="pass" id="exampleInputPassword1" class="form-control" placeholder="Password" required>
+                    <button type="submit" class="btn btn-primary">Sign in</button>
                 </form>
             </section>
         </div>
@@ -150,7 +166,7 @@ session_destroy();
                         </ul>
                     </div>
                 </div>
-                <p id="copy">
+                <p class="mt-5 mb-3 text-muted" id="copy">
                     &copy;Copy Rights . 2020-2021 つだぬまいんふぉ . All Rights Reserved
                     <img src="../img/つだぬまいんふぉ.png" width="80" height="50" alt="つだぬまいんふぉ">
                 </p>
