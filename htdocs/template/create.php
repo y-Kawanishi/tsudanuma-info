bcbb1cf2311ce13512e20daa77e992c251d2cfa0
y-Kawanishi <s1942031UM@s.chibakoudai.jp>
2021-03-30 17:57:39 +0900

    新規会員登録画面編集

--- htdocs/template/create.php
+++ htdocs/template/create.php
@@ -11,16 +11,35 @@ if( isset($_SESSION['err_msg']) ){
         <meta charset="utf-8" />
         <!-- css -->
         <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
         <!-- bootstrap -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sign-in/">
         <!-- font -->
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Kosugi&family=Kosugi+Maru&display=swap" rel="stylesheet">
         <title>新規会員登録</title>
+
+        <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+        
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+        </style>
+
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -73,37 +92,33 @@ if( isset($_SESSION['err_msg']) ){
         <div id="wrapper" class="container w-50">
         <div id="page_top"><a href="#"></a></div>
             <section class="section">
-                <h1>新規会員登録</h1>
                 <form id="entry" method="POST" action="./check.php">
-                    <div class="mb-3">
-                        <label for="exampleInputName1" class="form-label">お名前<span class="must"></span></label>
-                        <input type="text" name="name" class="form-control" id="exampleInputName1" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">メールアドレス<span class="must"></span></label>
-                        <input type="email" name="email" class="form-control" id="exampleInputEmail1" required aria-describedby="emailHelp">
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">パスワード<span class="must"></span></label>
-                        <input type="password" name="pass" class="form-control" id="exampleInputPassword1" required>
-                        <div id="PasswordHelp" class="form-text">※4桁以上で入力してください。</div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword2" class="form-label">パスワード(確認)<span class="must"></span></label>
-                        <input type="password" name="pass2" class="form-control" id="exampleInputPassword2" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputSex" class="form">生年月日<span class="must"></span></label><br>
-                        年<input type="number" name="year" class="form-control"　id="exampleInputSex1" required>
-                        月<input type="number" name="month" class="form-control"　id="exampleInputSex2" required>
-                        日<input type="number" name="day" class="form-control"　id="exampleInputSex3" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputGender" class="form-label">性別<span class="must"></span></label><br>
-                        <input type="radio" name="gender" class="form-label" id="exampleInputSex1" value="男性" required> 男性 
-                        <input type="radio" name="gender" class="form-label" id="exampleInputSex2" value="女性"> 女性 
-                        <input type="radio" name="gender" class="form-label" id="exampleInputSex3" value="その他"> その他 
-                    </div>
+                    <img class="mb-4" src="../img/つだぬまいんふぉ2.png" alt="" width="146" height="72">
+                    <h1 class="h3 mb-3 fw-normal">新規会員登録</h1>
+                    <!-- name -->
+                    <label for="exampleInputName1" class="form-label"><span class="must"></span></label>
+                    <input type="text" name="name" id="exampleInputName1" class="form-control" placeholder="Name" required autofocus>
+                    <!-- email -->
+                    <label for="exampleInputEmail1" class="form-label"><span class="must"></span></label>
+                    <input type="email" name="email" id="exampleInputEmail1" class="form-control" placeholder="Email address" required autofocus>
+                    <!-- pass -->
+                    <label for="exampleInputPassword1" class="form-label"><span class="must"></span></label>
+                    <input type="password" name="pass" id="exampleInputPassword1" class="form-control" placeholder="Password" required autofocus>
+                    <div id="PasswordHelp" class="form-text">※4桁以上で入力してください。</div>
+                    <label for="exampleInputPassword2" class="form-label"><span class="must"></span></label>
+                    <input type="password" name="pass2" id="exampleInputPassword2" class="form-control" placeholder="Password" required autofocus>
+                    <div id="PasswordHelp" class="form-text">※同じパスワードを入力してください。</div>
+                    <!-- 生年月日 -->
+                    <label for="exampleInputBirth" class="form"><span class="must"></span></label>
+                    年<input type="number" name="year" id="exampleInputBirth1" class="form-control" placeholder="Year" required autofocus>
+                    月<input type="number" name="month" id="exampleInputBirth2" class="form-control" placeholder="Month" required autofocus>
+                    日<input type="number" name="day" id="exampleInputBirth3" class="form-control" placeholder="Day" required autofocus>
+                    <!-- 性別 -->
+                    <label for="exampleInputGender" class="form-label"><span class="must"></span></label><br>
+                    <input type="radio" name="gender" id="exampleInputGender1" class="form-label" value="男性" required> 男性
+                    <input type="radio" name="gender" id="exampleInputGender2" class="form-label" value="女性"> 女性
+                    <input type="radio" name="gender" id="exampleInputGender3" class="form-label" value="その他"> その他
+                    <!-- 確認画面へ -->
                     <div class="d-grid gap-2 d-md-flex justify-content-md-end">
                         <button type="submit" class="btn btn-primary">確認画面へ</button>
                     </div>
