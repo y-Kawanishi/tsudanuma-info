07e282cf92ebb82b0b5d63e6978382320396c985
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-04-11 12:39:14 +0900

    店削除確認画面

--- htdocs/delete_data/index.php
+++ htdocs/delete_data/index.php
@@ -1,23 +1,38 @@
+<!DOCTYPE html>
+<html lang='ja'>
 <head>
-    <title><?=$title?>：店削除</title>
+    <meta charset='utf-8' />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Kosugi&family=Kosugi+Maru&display=swap" rel="stylesheet">
+    <title><?=$title?>：店確認</title>
+    <link rel="stylesheet" href="../css/buy.css">
 </head>
-<link rel="stylesheet" href="../css/buy.css">
-<div class="box3">
-    <p><?=$title?>：店削除</p>
-</div>
-<ul class="card-link">
+<body>
+<div class="container w-80">
+    <div class="box3">
+        <p><?=$title?>：店確認</p>
+    </div>
+    <ul class="card-link">
 <?php
 include "../dbconnect/pdo_connect.php";
 $sql = "SELECT * FROM $item";
 $stmt = $pdo -> query($sql);
+$stmt2 = $stmt;
 foreach($stmt as $row){
 ?>
-    <li><a href="./get.php?item=<?=$item?>&id=<?=$row['id']?>" class="card-link">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-paperclip" viewBox="0 0 16 16">
-        <path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0V3z"/>
-        </svg> <?=$row['brand']?> <?=$row['branch']?></a>
-    </li>
+        <li><a href="./get.php?item=<?=$item?>&id=<?=$row['id']?>" class="card-link">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-paperclip" viewBox="0 0 16 16">
+            <path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0V3z"/>
+            </svg> <?=$row['brand']?> <?=$row['branch']?></a>
+        </li>
 <?php
 }
 ?>
-</ul>
+    </ul>
+</div>
