a82d3e91b38de8d87623a1906adc97b35bbf9ae5
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-04-19 23:03:00 +0900

    口コミ確認画面

---
+++ htdocs/shop_comment/index.php
@@ -0,0 +1,19 @@
+<?php
+include "../dbconnect/pdo_connect.php";
+$sql = "SELECT * FROM $table JOIN $item JOIN user ON $table.shop_id = $item.id And $table.user_id = user.id";
+$stmt = $pdo -> query($sql);
+foreach($stmt as $row){
+?>
+    <p>No.<?=$row['num']?></p>
+    <p>ID:<?=$row['brand']?> <?=$row['branch']?></p>
+    <p>コメント：<?=$row['comment']?></p>
+    <p><?php
+        for($i = 0; $i < $row['star']; $i++){
+            echo "★";
+        }
+    ?></p>
+    <p><?=$row[16]?>さん</p>
+<?php
+// print_r($row);
+}
+?>
