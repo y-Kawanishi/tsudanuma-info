6f5a846ba5fbe77a36e4aa8ce80f3c8d80421fd1
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-04-25 22:10:36 +0900

    コメント閲覧画面 デザイン

--- htdocs/shop_comment/index.php
+++ htdocs/shop_comment/index.php
@@ -5,12 +5,15 @@ $sql = "SELECT * FROM $table JOIN $item JOIN user ON $table.shop_id = $item.id A
 $stmt = $pdo -> query($sql);
 foreach($stmt as $row){
 ?>
-    <div class="card w-25">
+    <div class="card w-25 shop">
         <h5 class="card-header"><?=$row['brand']?> <?=$row['branch']?></h5>
         <div class="card-body">
             <h5 class="card-title"><?php
                 for($i = 0; $i < $row['star']; $i++){
                     echo "★";
+                }
+                for($i = $row['star']; $i < 5; $i++){
+                    echo "☆";
                 }
             ?></h5>
             <p class="card-text"><?=$row['comment']?></p>
@@ -19,6 +22,13 @@ foreach($stmt as $row){
             <a href="#" class="btn btn-primary">Go shop</a>
         </div>
     </div>
+    <style>
+    .shop{
+        text-align: left;
+        float: left;
+        margin: auto;
+    }
+    </style>
 <?php
 // print_r($row);
 }
