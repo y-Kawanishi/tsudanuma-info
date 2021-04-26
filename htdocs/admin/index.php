0376fc76da9ea91634e00d0ea360522e0c968e06
y-Kawanishi <s1942031UM@s.chibakoudai.jp>
2021-04-26 13:22:28 +0900

    test

--- htdocs/admin/index.php
+++ htdocs/admin/index.php
@@ -6,8 +6,8 @@
         array("name" => "服の店削除・編集", "comment" => "登録したアパレル系店舗の編集や削除を行えます。", "path" => "../delete_data/apparel.php"), 
         array("name" => "食材の店追加", "comment" => "スーパーなど食料品の店の追加処理を行えます。", "path" => "../insert_data/food.php"), 
         array("name" => "食材の店削除・編集", "comment" => "登録した食料品の店の編集や削除を行えます。", "path" => "../delete_data/food.php"), 
-        array("name" => "雑貨の店追加", "comment" => "雑貨・日用品の店の追加処理を行えます。", "path" => "../insert_data/goods.php"), 
-        array("name" => "雑貨の店削除・編集", "comment" => "登録した雑貨・日用品の店の編集や削除を行えます。", "path" => "../delete_data/goods.php")
+        array("name" => "雑貨の店追加", "comment" => "雑貨の店の追加処理を行えます。", "path" => "../insert_data/goods.php"), 
+        array("name" => "雑貨の店削除・編集", "comment" => "登録した雑貨の店の編集や削除を行えます。", "path" => "../delete_data/goods.php")
     );
 ?>
 <div class="row">
